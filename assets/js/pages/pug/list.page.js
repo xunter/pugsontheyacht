305b55ea003310220efda9cd496f3e609c02d496
Pavel Nazarov <xunterrzn@gmail.com>
2019-10-07 02:40:21 +0300

    Pugs list, add, edit, delete features were implemented - next thing is db store integration

--- assets/js/pages/pug/list.page.js
+++ assets/js/pages/pug/list.page.js
@@ -3,6 +3,10 @@ parasails.registerPage('list', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
+    addingpug: { name: '', text: '' },
+    editingpug: { id: '', date: '', name: '', text: '' },
+    addnewpugmodal: false,
+    pugs: []
     //…
   },
 
@@ -22,5 +26,59 @@ parasails.registerPage('list', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
     //…
+    clickShowNewPugModal: function() {
+      //alert(this.addnewpugmodal);
+      this.addnewpugmodal = true;
+    },
+
+    clickCloseNewPugModal: function() {
+      //alert(this.addnewpugmodal);
+      this.addnewpugmodal = false;
+    },
+
+    clickSubmitAddNewPug: function() {
+
+    },
+
+    submitAddNewPug: function() {
+
+      this.addnewpugmodal = false;
+
+      console.log(this.addingpug);
+
+      this.pugs.push({ id: new Date().getTime(), date: new Date().toISOString(), name: this.addingpug.name, text: this.addingpug.text });
+
+
+
+    },
+
+    deletePug: function(deletingPug) {
+      let deletingIndex = this.pugs.indexOf(deletingPug);
+      console.log(deletingIndex);
+      this.pugs.splice(deletingIndex, 1);
+    },
+
+    editPug: function(editingPug) {
+      this.editingpug.id = editingPug.id;
+        this.editingpug.date = editingPug.date;
+          this.editingpug.name = editingPug.name;
+            this.editingpug.text = editingPug.text;
+    },
+
+    cancelEditPug: function(editingPug) {
+
+      this.editingpug.id = '';
+    },
+
+    saveEditingPug: function(editingPug) {
+      var self = this;
+      let editingPugModel = this.pugs.find(function(p) { return p.id === self.editingpug.id; });
+      this.editingpug.id = '';
+
+      editingPugModel.name = this.editingpug.name;
+      editingPugModel.text = this.editingpug.text;
+    }
+
+
   }
 });
